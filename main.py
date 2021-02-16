623844a906d3d1d07257dab4bc272635ea698c1c
Carlos Jaramillo <carlos87jaramillo@gmail.com>
2021-02-15 20:31:18 -0500

    git commit

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Proyecto python y MySql:
+- Abrir asistente.
+- Login o registro.
+- Si elegimos registro, crear un usuario en la db.
+- Si elegimos login, identificar el usuario.
+- Crear, mostrar y eliminar notas.
+"""
